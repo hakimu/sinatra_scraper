1aa142c92c960ce491ce9f656725b095a3e82eb6
Hakimu Jackson <hakimu@newrelic.com>
2016-07-10 23:48:19 -0700

    Need to fix database.  Not saving all songs. Need to refactor song.rb

--- app.rb
+++ app.rb
@@ -1,39 +1,17 @@
 require 'json'
-require 'nokogiri'
-require 'httparty'
 require './song'
 require './lib/scraper'
 require 'shotgun'
 require 'pry'
-require 'data_mapper'
-require 'dm-sqlite-adapter'
 require 'sinatra'
 require 'newrelic_rpm'
 
-# DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/scraped_songs.db")
-
-# class Song
-
-#   include DataMapper::Resource
-#   property :id, Serial
-#   property :song_title, String
-#   property :artist, String
-#   property :created_at, DateTime
-#   property :updated_at, DateTime
-
-# end
-
-# DataMapper.finalize.auto_upgrade!
-
 class NprBump < Sinatra::Base
 
-	get '/' do
-    @artists = NprScraper.new.parse_artist[0]
-    @titles = NprScraper.new.parse_song_title[0]
-    @stuff = Song.all
+  get '/' do
+    @songs = Song.all
 		erb :index
 	end
-
 
 end
 
--- song.rb
+++ song.rb
@@ -1,13 +1,8 @@
 require 'json'
-require 'nokogiri'
-require 'httparty'
 require './lib/scraper'
-require 'shotgun'
 require 'pry'
 require 'data_mapper'
 require 'dm-sqlite-adapter'
-require 'sinatra'
-require 'newrelic_rpm'
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/scraped_songs.db")
 
@@ -20,10 +15,19 @@ class Song
   property :created_at, DateTime
   property :updated_at, DateTime
 
+  def create_song
+    songs = Song.new
+    NprScraper.new.parse_song_title[0].each do |song|
+      songs.song_title = song
+    end
+    NprScraper.new.parse_artist[0].each do |artist|
+      songs.artist = artist
+    end
+    songs.created_at = DateTime.now
+    songs.updated_at = DateTime.now
+    songs.save
+  end
+
 end
 
 DataMapper.finalize.auto_upgrade!
-
-
-
-
